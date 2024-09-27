f00ffdda3a9df1c502669d800f1d72745776b7ce
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-09-27 20:30:05 +0700

    Implement CVPreview component to display user information
    - Add sections to preview personal details, education, and experience.
    - Use conditional rendering to display fields only if values are provided.
    - Integrate icons for phone, email, and address for better visual representation.
    - Style component using external CSS.

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -1,2 +1,89 @@
-function CVPreview() {}
+import "../styles/CVPreview.css";
+import AddressIcon from "../assets/AddressIcon.svg";
+import EmailIcon from "../assets/EmailIcon.svg";
+import TelephoneIcon from "../assets/TelephoneIcon.svg";
+
+function CVPreview({ personalDetails, education, experience }) {
+  return (
+    <>
+      <section className="personal-details-preview-container">
+        <div className="name-position">
+          <p>{personalDetails.fullName}</p>
+          <p>{personalDetails.appliedPosition}</p>
+        </div>
+
+        <div className="phone-email-address">
+          <div className="phone">
+            {personalDetails.phoneNumber && (
+              <>
+                <img src={TelephoneIcon} alt="telephone-icon" />
+                <p>{personalDetails.phoneNumber}</p>
+              </>
+            )}
+          </div>
+
+          <div className="email">
+            {personalDetails.email && (
+              <>
+                <img src={EmailIcon} alt="email-icon" />
+                <p>{personalDetails.email}</p>
+              </>
+            )}
+          </div>
+
+          <div className="address">
+            {personalDetails.address && (
+              <>
+                <img src={AddressIcon} alt="address-icon" />
+                <p>{personalDetails.address}</p>
+              </>
+            )}
+          </div>
+        </div>
+      </section>
+
+      <section className="education-preview-container">
+        <h2 className="education-preview-title">Education</h2>
+        <div className="education-details">
+          <div className="field-university-degree">
+            <p>{education.fieldOfStudy}</p>
+            <div className="university-degree">
+              {education.universityName && (
+                <p>
+                  {education.universityName}, {education.degreeTitle}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div className="start-end">
+            {education.startDate && (
+              <p>
+                {education.startDate}-{education.endDate}
+              </p>
+            )}
+          </div>
+        </div>
+      </section>
+
+      <section className="experience-preview-container">
+        <h2 className="experience-preview-title">Experience</h2>
+        <div className="experience-details">
+          <div className="company-position">
+            <p>{experience.positionTitle}</p>
+            <p>{experience.companyName}</p>
+          </div>
+
+          <div className="start-end">
+            {experience.startDate && (
+              <p>
+                {experience.startDate}-{experience.endDate}
+              </p>
+            )}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
 export default CVPreview;
