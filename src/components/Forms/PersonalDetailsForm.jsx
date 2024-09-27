010a4bc06f77d8ccefa404d7d49f962bd6d90ad5
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-09-27 20:28:03 +0700

    Refactor PersonalDetailsForm component to manage state and add toggle functionality
    - Add useState to handle the visibility of the personal details form.
    - Implement ToggleButton to toggle the form's open/closed state.
    - Update form fields to receive values and handle changes through props.
    - Add EditButton and ApplyButton components for form actions.

--- src/components/Forms/PersonalDetailsForm.jsx
+++ src/components/Forms/PersonalDetailsForm.jsx
@@ -1,35 +1,91 @@
-import "../../styles/Form.css";
+import { useState } from "react";
+import "../../styles/Forms.css";
+import ApplyButton from "../Buttons/ApplyButton.jsx";
+import EditButton from "../Buttons/EditButton.jsx";
+import ToggleButton from "../Buttons/ToggleButton.jsx";
 
-function PersonalDetailsForm() {
+function PersonalDetailsForm({ personalDetails, handleChange }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="personal-details-form-container">
-      <h2>Personal Details</h2>
-      <form>
-        <label>
-          Full Name
-          <input type="text" name="full-name" placeholder="Muhammad Iqbal" />
-        </label>
+    <section className="personal-details-form-container">
+      <div className="personal-details-form-title-toggle">
+        <h2 className="personal-details-form-title">Personal Details</h2>
+        <ToggleButton handleToggle={handleToggle} isOpen={isOpen} />
+      </div>
+      {isOpen && (
+        <form className="personal-details-form-details">
+          <label className="full-name-label">
+            Full Name
+            <input
+              className="full-name-input"
+              type="text"
+              name="fullName"
+              placeholder="John Doe"
+              value={personalDetails.fullName}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          Phone Number
-          <input type="tel" name="phone-number" placeholder="+6285864945923" />
-        </label>
+          <label className="applied-position-label">
+            Applied Position
+            <input
+              className="applied-position-input"
+              type="text"
+              name="appliedPosition"
+              placeholder="Fullstack Web Developer"
+              value={personalDetails.appliedPosition}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          Email
-          <input
-            type="email"
-            name="email"
-            placeholder="muhammadiqbal.090300@gmail.com"
-          />
-        </label>
+          <label className="phone-number-label">
+            Phone Number
+            <input
+              className="phone-number-input"
+              type="tel"
+              name="phoneNumber"
+              placeholder="5551234567"
+              value={personalDetails.phoneNumber}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          Address
-          <input type="text" name="address" placeholder="Bandung, West Java" />
-        </label>
-      </form>
-    </div>
+          <label className="email-label">
+            Email
+            <input
+              className="email-input"
+              type="email"
+              name="email"
+              placeholder="johndoe@gmail.com"
+              value={personalDetails.email}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label className="address-label">
+            Address
+            <input
+              className="address-input"
+              type="text"
+              name="address"
+              placeholder="New York, US"
+              value={personalDetails.address}
+              onChange={handleChange}
+            />
+          </label>
+
+          <div className="personal-details-form-buttons">
+            <EditButton />
+            <ApplyButton />
+          </div>
+        </form>
+      )}
+    </section>
   );
 }
 
