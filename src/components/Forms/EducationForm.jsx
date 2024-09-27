def5608414677a667e77040549e51eef5b6306dc
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-09-27 20:26:34 +0700

    Refactor EducationForm component to manage state and add toggle functionality
    - Add useState to handle the visibility of the education form.
    - Implement ToggleButton to toggle the form's open/closed state.
    - Update form fields to receive values and handle changes through props.
    - Add EditButton and ApplyButton components for form actions.

--- src/components/Forms/EducationForm.jsx
+++ src/components/Forms/EducationForm.jsx
@@ -1,39 +1,89 @@
-import "../../styles/Form.css";
+import { useState } from "react";
+import "../../styles/Forms.css";
+import ApplyButton from "../Buttons/ApplyButton.jsx";
+import EditButton from "../Buttons/EditButton.jsx";
+import ToggleButton from "../Buttons/ToggleButton.jsx";
 
-function EducationForm() {
+function EducationForm({ education, handleChange }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="education-form-container">
-      <h2>Education</h2>
-      <form>
-        <label>
-          University Name
-          <input
-            type="text"
-            name="university-name"
-            placeholder="UIN Sunan Gunung Djati Bandung"
-          />
-        </label>
+    <section className="education-form-container">
+      <div className="education-form-title-toggle">
+        <h2 className="education-form-title">Education</h2>
+        <ToggleButton handleToggle={handleToggle} isOpen={isOpen} />
+      </div>
+      {isOpen && (
+        <form className="education-form-details">
+          <label className="field-of-study-label">
+            Field of Study
+            <input
+              className="field-of-study-input"
+              type="text"
+              name="fieldOfStudy"
+              placeholder="Computer Science"
+              value={education.fieldOfStudy}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          Degree Title
-          <input
-            type="text"
-            name="degree-title"
-            placeholder="BSc in Sociology"
-          />
-        </label>
+          <label className="university-name-label">
+            University Name
+            <input
+              className="university-name-input"
+              type="text"
+              name="universityName"
+              placeholder="Massachusetts Institute of Technology"
+              value={education.universityName}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          Start Date
-          <input type="date" name="start-date" />
-        </label>
+          <label className="degree-title-label">
+            Degree Title
+            <input
+              className="degree-title-input"
+              type="text"
+              name="degreeTitle"
+              placeholder="BSc in Computer Science and Engineering"
+              value={education.degreeTitle}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          End Date
-          <input type="date" name="end-date" />
-        </label>
-      </form>
-    </div>
+          <label className="start-date-label">
+            Start Date
+            <input
+              className="start-date-input"
+              type="date"
+              name="startDate"
+              value={education.startDate}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label className="end-date-label">
+            End Date
+            <input
+              className="end-date-input"
+              type="date"
+              name="endDate"
+              value={education.endDate}
+              onChange={handleChange}
+            />
+          </label>
+
+          <div className="education-form-buttons">
+            <EditButton />
+            <ApplyButton />
+          </div>
+        </form>
+      )}
+    </section>
   );
 }
 
