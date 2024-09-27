92df45ea55b0a42dc882018bf818722b1f12edfb
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-09-27 20:28:53 +0700

    Refactor ExperienceForm component to manage state and add toggle functionality
    - Add useState to handle the visibility of the experience form.
    - Implement ToggleButton to toggle the form's open/closed state.
    - Update form fields to receive values and handle changes through props.
    - Add EditButton and ApplyButton components for form actions.

--- src/components/Forms/ExperienceForm.jsx
+++ src/components/Forms/ExperienceForm.jsx
@@ -1,36 +1,78 @@
-import "../../styles/Form.css";
+import { useState } from "react";
+import "../../styles/Forms.css";
+import ApplyButton from "../Buttons/ApplyButton.jsx";
+import EditButton from "../Buttons/EditButton.jsx";
+import ToggleButton from "../Buttons/ToggleButton.jsx";
 
-function Experience() {
+function ExperienceForm({ experience, handleChange }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="experience-form-container">
-      <h2>Experience</h2>
-      <form>
-        <label>
-          Company Name
-          <input type="text" name="company-name" placeholder="Google" />
-        </label>
+    <section className="experience-form-container">
+      <div className="experience-form-title-toggle">
+        <h2 className="experience-form-title">Experience</h2>
+        <ToggleButton handleToggle={handleToggle} isOpen={isOpen} />
+      </div>
+      {isOpen && (
+        <form className="experience-form-details">
+          <label className="position-title-label">
+            Position Title
+            <input
+              className="position-title-input"
+              type="text"
+              name="positionTitle"
+              placeholder="Frontend Web Developer"
+              value={experience.positionTitle}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          Position Title
-          <input
-            type="text"
-            name="position-title"
-            placeholder="Web Developer"
-          />
-        </label>
+          <label className="company-name-label">
+            Company Name
+            <input
+              className="company-name-input"
+              type="text"
+              name="companyName"
+              placeholder="Google"
+              value={experience.companyName}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          Start Date
-          <input type="date" name="start-date" />
-        </label>
+          <label className="start-date-label">
+            Start Date
+            <input
+              className="start-date-input"
+              type="date"
+              name="startDate"
+              value={experience.startDate}
+              onChange={handleChange}
+            />
+          </label>
 
-        <label>
-          End Date
-          <input type="date" name="end-date" />
-        </label>
-      </form>
-    </div>
+          <label className="end-date-label">
+            End Date
+            <input
+              className="end-date-input"
+              type="date"
+              name="endDate"
+              value={experience.endDate}
+              onChange={handleChange}
+            />
+          </label>
+
+          <div className="experience-form-buttons">
+            <EditButton />
+            <ApplyButton />
+          </div>
+        </form>
+      )}
+    </section>
   );
 }
 
-export default Experience;
+export default ExperienceForm;
