6291f519f083e10487419f61a34756f23949305a
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-10-07 21:52:13 +0700

    Merge button components into one file and refactor to use CSS Modules

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,32 @@
+import ChevronIcon from "../assets/ChevronIcon.svg";
+import styles from "../styles/Buttons.module.css";
+
+export function ApplyButton() {
+  return (
+    <button className={styles.applyButton} type="submit">
+      Apply
+    </button>
+  );
+}
+
+export function EditButton({ onEdit }) {
+  return (
+    <button className={styles.editButton} type="button" onClick={onEdit}>
+      Edit
+    </button>
+  );
+}
+
+export function ToggleButton({ isOpen, onToggle }) {
+  return (
+    <button className={styles.toggleButton} onClick={onToggle}>
+      <img
+        className={
+          isOpen ? `${styles.chevronIcon} ${styles.rotate}` : styles.chevronIcon
+        }
+        src={ChevronIcon}
+        alt="Chevron Icon"
+      />
+    </button>
+  );
+}
