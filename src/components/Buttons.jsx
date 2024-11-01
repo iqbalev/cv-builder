de1e4dffa4846905e692e42437318aa3713929b0
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-11-01 19:45:37 +0700

    Add cancel button component and change "add" to plus icon

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,18 +1,11 @@
 import ChevronIcon from "../assets/ChevronIcon.svg";
+import PlusIcon from "../assets/PlusIcon.svg";
 import styles from "../styles/Buttons.module.css";
 
-export function ApplyButton() {
+export function AddButton({ onAdd }) {
   return (
-    <button className={styles.applyButton} type="submit">
-      Apply
-    </button>
-  );
-}
-
-export function EditButton({ onEdit }) {
-  return (
-    <button className={styles.editButton} type="button" onClick={onEdit}>
-      Edit
+    <button className={styles.addButton} type="button" onClick={onAdd}>
+      <img className={styles.plusIcon} src={PlusIcon} alt="Plus Icon" />
     </button>
   );
 }
@@ -31,10 +24,26 @@ export function ToggleButton({ isOpen, onToggle }) {
   );
 }
 
-export function AddButton({ onAdd }) {
+export function EditButton({ onEdit }) {
   return (
-    <button className={styles.addButton} type="button" onClick={onAdd}>
-      Add
+    <button className={styles.editButton} type="button" onClick={onEdit}>
+      Edit
+    </button>
+  );
+}
+
+export function ApplyButton() {
+  return (
+    <button className={styles.applyButton} type="submit">
+      Apply
+    </button>
+  );
+}
+
+export function CancelButton({ onCancel }) {
+  return (
+    <button className={styles.cancelButton} type="button" onClick={onCancel}>
+      Cancel
     </button>
   );
 }
