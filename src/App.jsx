746af174f4511ed3d897d952bdab9d0c8b18f98d
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-11-02 08:42:55 +0700

    Refactor App component to support multiple education and experience entries
    - Add handleAddEducation and handleAddExperience functions to enable new entries.
    - Update handleTempStatesChange and handleStatesApply to support multiple entries by index, allowing edits without affecting main state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-import EducationSection from "./components/EducationSection";
-import ExperienceSection from "./components/ExperienceSection";
-import PersonalDetailsSection from "./components/PersonalDetailsSection";
+import EducationSection from "./components/Education/EducationSection";
+import ExperienceSection from "./components/Experience/ExperienceSection";
+import PersonalDetailsSection from "./components/PersonalDetails/PersonalDetailsSection";
 import ResumePreview from "./components/ResumePreview";
 
 function App() {
+  /* 
+  - These are the FINAL states that will be use when user clicks on apply button 
+  - The data rendered on ResumePreview are comes from these states
+  */
   const [personalDetails, setPersonalDetails] = useState({
     fullName: "John Doe",
     jobTitle: "Fullstack Web Developer",
@@ -14,49 +18,107 @@ function App() {
     address: "New York, US",
   });
 
+  const [education, setEducation] = useState([
+    {
+      fieldOfStudy: "Computer Science",
+      school: "Oxford University",
+      degree: "BSc in Computer Science and Engineering",
+      startDate: "2018-09",
+      isCurrentlyActive: false,
+      endDate: "2022-10",
+    },
+  ]);
+
+  const [experience, setExperience] = useState([
+    {
+      jobTitle: "Frontend Web Developer",
+      company: "Google",
+      startDate: "2023-03",
+      isCurrentlyActive: false,
+      endDate: "2024-01",
+      responsibilities: [
+        "Implementing UI/UX designs provided by the design team",
+        "Consuming and rendering APIs from the backend",
+        "Creating a responsive and interactive website",
+      ],
+    },
+  ]);
+
+  /* 
+  - These are the TEMP states that will hold the data before the user clicks on apply button
+  - They will allow user to make changes in edit mode without affecting the main states
+  */
   const [tempPersonalDetails, setTempPersonalDetails] =
     useState(personalDetails);
-
-  const [education, setEducation] = useState({
-    fieldOfStudy: "Computer Science",
-    university: "Oxford University",
-    degree: "BSc in Computer Science and Engineering",
-    startDate: "2018-09",
-    isCurrentlyActive: false,
-    endDate: "2022-10",
-  });
-
   const [tempEducation, setTempEducation] = useState(education);
-
-  const [experience, setExperience] = useState({
-    jobTitle: "Frontend Web Developer",
-    company: "Google",
-    startDate: "2023-03",
-    isCurrentlyActive: false,
-    endDate: "2024-01",
-    responsibilities: [
-      "Implementing UI/UX designs provided by the design team",
-      "Consuming and rendering APIs from the backend",
-      "Creating a responsive and interactive website",
-    ],
-  });
-
   const [tempExperience, setTempExperience] = useState(experience);
 
-  function handleTempStatesChange(e, setState) {
+  function handleTempStatesChange(e, index, setState) {
     const { name, value, checked } = e.target;
 
-    if (name === "isCurrentlyActive") {
-      setState((prev) => ({ ...prev, [name]: checked }));
-    } else if (name === "responsibilities") {
-      setState((prev) => ({ ...prev, [name]: value.split("\n") }));
+    setState((prev) => {
+      if (index !== undefined) {
+        const updatedArray = [...prev];
+        updatedArray[index] = {
+          ...updatedArray[index],
+          [name]:
+            name === "isCurrentlyActive"
+              ? checked
+              : name === "responsibilities"
+              ? value.split("\n")
+              : value,
+        };
+        return updatedArray;
+      }
+
+      if (name === "isCurrentlyActive") {
+        return { ...prev, [name]: checked };
+      } else if (name === "responsibilities") {
+        return { ...prev, [name]: value.split("\n") };
+      } else {
+        return { ...prev, [name]: value };
+      }
+    });
+  }
+
+  function handleStatesApply(setState, state, index) {
+    if (index !== undefined) {
+      setState((prev) => {
+        const updatedArray = [...prev];
+        updatedArray[index] = state[index];
+        return updatedArray;
+      });
     } else {
-      setState((prev) => ({ ...prev, [name]: value }));
+      setState(state);
     }
   }
 
-  function handleStatesApply(setState, state) {
-    setState(state);
+  function handleAddEducation() {
+    setTempEducation((prev) => [
+      ...prev,
+      {
+        fieldOfStudy: "",
+        school: "",
+        degree: "",
+        startDate: "",
+        isCurrentlyActive: false,
+        endDate: "",
+      },
+    ]);
+  }
+
+  function handleAddExperience() {
+    setTempExperience((prev) => [
+      ...prev,
+      {
+        jobTitle: "",
+        company: "",
+        startDate: "",
+        isCurrentlyActive: false,
+        endDate: "",
+        responsibilities: [],
+      },
+    ]);
   }
 
   return (
@@ -64,20 +126,34 @@ function App() {
       <div className="sections-container">
         <PersonalDetailsSection
           personalDetails={tempPersonalDetails}
-          onChange={(e) => handleTempStatesChange(e, setTempPersonalDetails)}
+          onChange={(e) =>
+            handleTempStatesChange(e, undefined, setTempPersonalDetails)
+          }
           onApply={() =>
             handleStatesApply(setPersonalDetails, tempPersonalDetails)
           }
         />
+
         <EducationSection
           education={tempEducation}
-          onChange={(e) => handleTempStatesChange(e, setTempEducation)}
-          onApply={() => handleStatesApply(setEducation, tempEducation)}
+          onChange={(e, index) =>
+            handleTempStatesChange(e, index, setTempEducation)
+          }
+          onApply={(index) =>
+            handleStatesApply(setEducation, tempEducation, index)
+          }
+          onAdd={handleAddEducation}
         />
+
         <ExperienceSection
           experience={tempExperience}
-          onChange={(e) => handleTempStatesChange(e, setTempExperience)}
-          onApply={() => handleStatesApply(setExperience, tempExperience)}
+          onChange={(e, index) =>
+            handleTempStatesChange(e, index, setTempExperience)
+          }
+          onApply={(index) =>
+            handleStatesApply(setExperience, tempExperience, index)
+          }
+          onAdd={handleAddExperience}
         />
       </div>
 
