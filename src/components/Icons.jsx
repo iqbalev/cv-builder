a72d1c7b754b1983779af4eb5d91a31b5e76d2fc
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-11-19 13:05:40 +0700

    Rename and add icon components
    Rename ChevronToggleIcon to ChevronIcon.
    Rename PlusMinToggleIcon to PlusMinIcon.
    Add AddressIcon component.
    Add EmailIcon component.
    Add EyeIcon component.
    Add PencilIcon component.
    Add PhoneIcon component.
    Add TickIcon component.
    Add TrashIcon component.

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,26 +1,63 @@
-import ChevronIcon from "../assets/ChevronIcon.svg";
-import MinIcon from "../assets/MinIcon.svg";
-import PlusIcon from "../assets/PlusIcon.svg";
-import styles from "../styles/ToggleIcons.module.css";
+import AddressSVG from "../assets/AddressIcon.svg";
+import ChevronSVG from "../assets/ChevronIcon.svg";
+import EmailSVG from "../assets/EmailIcon.svg";
+import EyeSVG from "../assets/EyeIcon.svg";
+import MinSVG from "../assets/MinIcon.svg";
+import PencilSVG from "../assets/PencilIcon.svg";
+import PhoneSVG from "../assets/PhoneIcon.svg";
+import PlusSVG from "../assets/PlusIcon.svg";
+import TickSVG from "../assets/TickIcon.svg";
+import TrashSVG from "../assets/TrashIcon.svg";
+import styles from "../styles/Icons.module.css";
 
-export function ChevronToggleIcon({ isOpen }) {
+export function AddressIcon() {
+  return (
+    <img className={styles.addressIcon} src={AddressSVG} alt="Address Icon" />
+  );
+}
+
+export function ChevronIcon({ isOpen }) {
   return (
     <img
       className={
         isOpen ? `${styles.chevronIcon} ${styles.rotate}` : styles.chevronIcon
       }
-      src={ChevronIcon}
+      src={ChevronSVG}
       alt="Chevron Icon"
     />
   );
 }
 
-export function PlusMinToggleIcon({ isOpen }) {
+export function EmailIcon() {
+  return <img className={styles.emailIcon} src={EmailSVG} alt="Email Icon" />;
+}
+
+export function EyeIcon() {
+  return <img className={styles.eyeIcon} src={EyeSVG} alt="View Icon" />;
+}
+
+export function PencilIcon() {
+  return <img className={styles.pencilIcon} src={PencilSVG} alt="Edit Icon" />;
+}
+
+export function PhoneIcon() {
+  return <img className={styles.phoneIcon} src={PhoneSVG} alt="Phone Icon" />;
+}
+
+export function PlusMinIcon({ isOpen }) {
   return (
     <img
       className={styles.plusMinIcon}
-      src={isOpen ? MinIcon : PlusIcon}
-      alt=""
+      src={isOpen ? MinSVG : PlusSVG}
+      alt="Plus/Min Icon"
     />
   );
+}
+
+export function TickIcon() {
+  return <img className={styles.tickIcon} src={TickSVG} alt="Apply Icon" />;
+}
+
+export function TrashIcon() {
+  return <img className={styles.trashIcon} src={TrashSVG} alt="Delete Icon" />;
 }
